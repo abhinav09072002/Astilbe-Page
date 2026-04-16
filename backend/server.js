a5dca24d758cb99565ed041137853d4f50a60bf4
abhinav09072002 <akv09072002@gamil.com>
2026-04-16 14:24:12 +0530

    Fix CORS origin for production domain

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:5173',
-  'https://astilbe-page.netlify.app', 
+  'https://astilbe-page.netlify.app'
+  'https://astilbepage.com', 
 ]
 
 console.log('🔗 Allowed CORS Origins:', allowedOrigins)
